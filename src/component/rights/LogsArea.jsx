5aaa9abf4ca33ba78a928f11df8bb85a7de0ead1
zhangyu <zhangyu@zhangyudeMBP.lan>
2016-07-03 00:00:20 +0800

    把测试功能添加到正式界面中

--- src/component/rights/LogsArea.jsx
+++ src/component/rights/LogsArea.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {tr} from '../../lib/Utils'
+import {saveString} from '../../lib/FileUtil';
 import { Icon,Button } from 'antd';
 import ExtraButton from './ExtraButton.jsx';
 import ExtraQrCode from './ExtraQrCode.jsx';
 import {killRaplog} from '../../lib/Commands'
 // import {getIpAddress,getAvailablePort} from '../../lib/Files';
-import {sendLogCommand,addOutputCooked,ADD_LOG,commonCommand,CLEAN_RAP_LOG} from '../../actions/AppActions.jsx'
+import {sendLogCommand,addOutputCooked,ADD_LOG,commonCommand,CLEAN_RAP_LOG,showAlert} from '../../actions/AppActions.jsx'
+import {PanelSaveLog} from '../Alerts.jsx';
 var cfg = require('../../config');
 class LogsArea  extends React.Component {
     constructor(props){
@@ -34,29 +36,6 @@ class LogsArea  extends React.Component {
         this.socket.on('connect', function () {
             console.log('----connection2----');
         });
-
-
-        // var ip = getIpAddress();
-        // getAvailablePort((port) => {
-        //     port = 8081;
-        //     var url = `http://${ip}:${port}`;
-        //     this.setState({qr:url})
-        //     console.log(`Server  ${ip}:${port}`)
-        //     var child_process = require("child_process");
-        //     var childProcess = child_process.spawn("node", [`./app/server/RapLogServer.js`, ip, port]);
-        //
-        //     childProcess.stdout.on('data', function (data) {
-        //         console.log('data:',data.toString())
-        //     })
-        //     childProcess.stdout.on('err', function (err) {
-        //         console.log('err:',err.toString())
-        //     })
-        //     this.socket = io.connect(url, { reconnection: true, reconnectionDelay: 1000 });
-        //     cfg.socket = this.socket;
-        //     this.socket.on('connect', function () {
-        //         console.log('----connection2----');
-        //     });
-        // });
         this.setPositionAtBottom();
     }
     componentWillMount() {
@@ -83,15 +62,10 @@ class LogsArea  extends React.Component {
                     commonCommand(CLEAN_RAP_LOG);
                     addOutputCooked(tr(200,tr(16)), true,ADD_LOG);//200 执行命令：xxxx
                 } } tr={16} iconName="delete"/>
-                <ExtraButton  onClick={() => {} } tr={17} iconName="save"/>
+                <ExtraButton  onClick={()=>{this.saveLog(); }} tr={17} iconName="save"/>
 
             </div>))
     }
-// <Button onClick={() => { console.log(this) } }><Icon type="caret-circle-o-right"/></Button>
-// <Button><Icon type="cross-circle-o"/></Button>
-//     <Button><Icon type="delete"/></Button>
-//     <Button><Icon type="save"/></Button>
-
     componentDidUpdate() {
         this.setPositionAtBottom();
     }
@@ -99,6 +73,24 @@ class LogsArea  extends React.Component {
     setPositionAtBottom(){
         var ex = document.getElementById("rapLogArea");//定位到最下面一行
         ex.scrollTop = ex.scrollHeight;
+    }
+    saveLog(){
+        var self = this;
+        showAlert(PanelSaveLog, function (path) {
+            var txt = self.props.logContent;
+            // console.log('<br>')
+            txt = txt.replace(/<br>/g,"\r\n")
+            txt= txt.replace(/<[^>]+>/g,"");//可以匹配<script></style></body>等，并置空。而不是替换<和>两个符号
+            txt = txt.replace(/&amp;/g,"&");//把 &amp; 替换成&;
+            txt = txt.replace(/&lt;/g,"<");//把 &lt; 替换成<
+            txt = txt.replace(/&gt;/g,">");//把 &gt; 替换成>
+            txt = txt.replace(/&nbsp;/g," ");//把 &nbsp; 替换成空格
+            // fs.writeFileSync(filePath, str, charset);
+            saveString(path, txt);
+            addOutputCooked(tr(22), true, ADD_LOG);//22 保存日志成功
+            addOutputCooked(path, true,ADD_LOG);//22 保存日志成功
+            // console.log('showAlertEnd:', value);
+        })
     }
     render(){
         //52-- rap log 日志 --
