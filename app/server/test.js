76aa15834597df227549e20bef9fdfa3c7652cdf
zhangyu <runinspring@gmail.com>
2016-08-10 17:09:00 +0800

    add font

--- app/server/test.js
+++ app/server/test.js
@@ -1,6 +1,6 @@
 var child_process = require("child_process");
 const p = child_process.fork(
-  'E:/ZhiHuaSiStudio/2016/RuffHelperGit/app/server/RapLogServer.js', ["10.0.2.106",9254]
+  'E:/ZhiHuaSiStudio/2016/RuffHelperGit/app/server/RapLogServer.js', ["10.0.2.164",9254]
 );
 p.on('message',m=>{
 	console.log('message in child2',m)
